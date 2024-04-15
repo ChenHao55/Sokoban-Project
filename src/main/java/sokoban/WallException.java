be3b77212fd657923a6dfdc0d9240919ebf2a6f9
andy.ying <andy.ying@alumnos.upm.es>
2024-04-15 17:52:01 +0200

    added WallException

---
+++ src/main/java/sokoban/WallException.java
@@ -0,0 +1,9 @@
+package sokoban;
+
+public class WallException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public WallException(String message) {
+		super(message);
+	}
+}
