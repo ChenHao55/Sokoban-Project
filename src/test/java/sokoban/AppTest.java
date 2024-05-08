0aaaa5012499ee3109332fdbb994ba15cda9bbf7
andy.ying <andy.ying@alumnos.upm.es>
2024-05-08 10:57:55 +0200

    Unitary Tests for beans

--- src/test/java/sokoban/AppTest.java
+++ src/test/java/sokoban/AppTest.java
@@ -10,6 +10,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import model.beans.Box;
+import model.beans.GoalPosition;
+import model.beans.WarehouseMan;
 import model.exceptions.IlegalPositionException;
 
 public class AppTest {
@@ -41,19 +43,49 @@ public class AppTest {
 		@Test
 		void invalidWarehouseManCreation() {
 			log.info("Executing test to check the incorrect creation of a WarehouseMan Object");
-			assertThrows(IlegalPositionException.class, () -> new Box(-1, -3));
+			assertThrows(IlegalPositionException.class, () -> new WarehouseMan(-1, -1));
 		}
 		
 		@Test
 		void invalidXWarehouseManCoord() {
 			log.info("Trying to create a WarehouseMan Object with invalid X coordinate");
-			assertThrows(IlegalPositionException.class, () -> new Box(-1, 6));
+			assertThrows(IlegalPositionException.class, () -> new WarehouseMan(-1, 1));
 		}
 		
 		@Test
 		void invalidYWarehouseManCoord() {
 			log.info("Trying to create a WarehouseMan Object with invalid Y coordinate");
-			assertThrows(IlegalPositionException.class, () -> new Box(6, -3));
+			assertThrows(IlegalPositionException.class, () -> new WarehouseMan(1, -1));
+		}
+		
+		@Test
+		void invalidGoalCreation() {
+			log.info("Executing test to check the incorrect creation of a GoalPosition Object");
+			assertThrows(IlegalPositionException.class, () -> new GoalPosition(-1, -1));
+		}
+		
+		@Test
+		void invalidLeftActionCreation() {
+			log.info("Executing test to check the incorrect creation of a LeftAction Object");
+			assertThrows(IlegalPositionException.class, () -> new WarehouseMan(-1, -1));
+		}
+		
+		@Test
+		void invalidRightActionCreation() {
+			log.info("Executing test to check the incorrect creation of a RightAction Object");
+			assertThrows(IlegalPositionException.class, () -> new WarehouseMan(-1, -1));
+		}
+		
+		@Test
+		void invalidUpActionCreation() {
+			log.info("Executing test to check the incorrect creation of a UpAction Object");
+			assertThrows(IlegalPositionException.class, () -> new WarehouseMan(-1, -1));
+		}
+		
+		@Test
+		void invalidDownActionCreation() {
+			log.info("Executing test to check the incorrect creation of a DownAction Object");
+			assertThrows(IlegalPositionException.class, () -> new WarehouseMan(-1, -1));
 		}
 	}
 	
@@ -70,7 +102,37 @@ public class AppTest {
 		@Test
 		void correctWarehouseManCreation() {
 			log.info("Executing test to check the correct creation of a WarehouseMan Object");
-			assertDoesNotThrow(() -> new Box(3, 4));
+			assertDoesNotThrow(() -> new WarehouseMan(3, 4));
+		}
+		
+		@Test
+		void correctGoalCreation() {
+			log.info("Executing test to check the incorrect creation of a GoalPosition Object");
+			assertThrows(IlegalPositionException.class, () -> new GoalPosition(1, 1));
+		}
+		
+		@Test
+		void correctLeftActionCreation() {
+			log.info("Executing test to check the incorrect creation of a LeftAction Object");
+			assertThrows(IlegalPositionException.class, () -> new WarehouseMan(1, 1));
+		}
+		
+		@Test
+		void correctRightActionCreation() {
+			log.info("Executing test to check the incorrect creation of a RightAction Object");
+			assertThrows(IlegalPositionException.class, () -> new WarehouseMan(1, 1));
+		}
+		
+		@Test
+		void correctUpActionCreation() {
+			log.info("Executing test to check the incorrect creation of a UpAction Object");
+			assertThrows(IlegalPositionException.class, () -> new WarehouseMan(1, 1));
+		}
+		
+		@Test
+		void correctDownActionCreation() {
+			log.info("Executing test to check the incorrect creation of a DownAction Object");
+			assertThrows(IlegalPositionException.class, () -> new WarehouseMan(1, 1));
 		}
 	}
 	
